1c854e5f59f96ab026a4e57e7a74fe06db053ff2
D2 <lcamelocasagrande@gmail.com>
2020-04-17 15:29:22 -0300

    Export requirements to an external file.

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
+from os import path
 
-setup(name='batsim_py',
-      author='lccasagrande',
-      version='0.1',
-      python_requires='>=3.7',
-      install_requires=[
-              'zmq',
-              'procset',
-              'pydispatcher',
-              'evalys'
-      ])
+
+def read_requirements_file(filename):
+    file = '%s/%s' % (path.dirname(path.realpath(__file__)), filename)
+    with open(file) as f:
+        return [line.strip() for line in f]
+
+
+setup(
+    name='batsim_py',
+    author='lccasagrande',
+    version='0.1',
+    python_requires='>=3.7',
+    install_requires=read_requirements_file('requirements.txt'),
+)
