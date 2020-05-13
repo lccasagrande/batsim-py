9d2c439ec2d8c7fc7c51ca179280375164fcebc8
D2 <lcamelocasagrande@gmail.com>
2020-05-13 16:28:58 -0300

    Remove monitors ctr dependency.

--- batsim_py/monitors.py
+++ batsim_py/monitors.py
@@ -2,6 +2,7 @@
 from abc import ABC, abstractmethod
 from itertools import groupby
 import time as tm
+from typing import Optional
 
 import pandas as pd
 from procset import ProcSet
@@ -19,16 +20,7 @@ from .simulator import SimulatorHandler
 
 
 class Monitor(ABC):
-    """ Simulation Monitor base class.
-
-    Args:
-        simulator: the simulator handler.
-    """
-
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        assert isinstance(simulator, SimulatorHandler)
-        self._simulator = simulator
-
+    """ Simulation Monitor base class. """
     @property
     @abstractmethod
     def info(self) -> dict:
@@ -54,13 +46,9 @@ class JobMonitor(Monitor):
     """ Simulation Job Monitor class.
 
     This monitor collects statistics from each job submitted to the system.
-
-    Args:
-        simulator: the simulator handler.
     """
 
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        super().__init__(simulator)
+    def __init__(self) -> None:
         self.__info: dict = {
             'job_id': [],
             'workload_name': [],
@@ -80,11 +68,8 @@ class JobMonitor(Monitor):
             'consumed_energy': [],
         }
 
-        subscribe(
-            self.__on_simulation_begins,
-            SimulatorEvent.SIMULATION_BEGINS,
-            self._simulator,
-        )
+        subscribe(self.__on_simulation_begins,
+                  SimulatorEvent.SIMULATION_BEGINS)
 
         subscribe(self.__update_info, JobEvent.COMPLETED)
 
@@ -112,22 +97,23 @@ class JobMonitor(Monitor):
 
     def __update_info(self, sender: Job) -> None:
         """ Record job statistics. """
+        alloc = ProcSet(*sender.allocation) if sender.allocation else None
+        success = int(sender.state == JobState.COMPLETED_SUCCESSFULLY)
         self.__info['job_id'].append(sender.id)
         self.__info['workload_name'].append(sender.workload)
         self.__info['profile'].append(sender.profile.name)
         self.__info['submission_time'].append(sender.subtime)
         self.__info['requested_number_of_resources'].append(sender.res)
         self.__info['requested_time'].append(sender.walltime)
-        self.__info['success'].append(
-            int(sender.state == JobState.COMPLETED_SUCCESSFULLY))
+        self.__info['success'].append(success)
         self.__info['final_state'].append(str(sender.state))
         self.__info['starting_time'].append(sender.start_time)
         self.__info['execution_time'].append(sender.runtime)
         self.__info['finish_time'].append(sender.stop_time)
         self.__info['waiting_time'].append(sender.waiting_time)
         self.__info['turnaround_time'].append(sender.turnaround_time)
-        self.__info['stretch'].append(sender.slowdown)
-        self.__info['allocated_resources'].append(ProcSet(*sender.allocation))
+        self.__info['stretch'].append(sender.stretch)
+        self.__info['allocated_resources'].append(alloc)
         self.__info['consumed_energy'].append(-1)
 
 
@@ -136,12 +122,12 @@ class SchedulerMonitor(Monitor):
 
     This monitor collects statistics about the scheduler performance.
 
-    Args:
-        simulator: the simulator handler.
+
+
     """
 
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        super().__init__(simulator)
+    def __init__(self) -> None:
+
         self.__info: dict = {
             'makespan': 0,
             'max_slowdown': 0,
@@ -159,17 +145,16 @@ class SchedulerMonitor(Monitor):
             'nb_jobs_rejected': 0,
             'nb_jobs_success': 0,
         }
+        self.__simulator: Optional[SimulatorHandler] = None
 
         subscribe(
             self.__on_simulation_begins,
             SimulatorEvent.SIMULATION_BEGINS,
-            self._simulator,
         )
 
         subscribe(
             self.__on_simulation_ends,
             SimulatorEvent.SIMULATION_ENDS,
-            self._simulator,
         )
 
         subscribe(self.__on_job_completed, JobEvent.COMPLETED)
@@ -197,6 +182,7 @@ class SchedulerMonitor(Monitor):
 
     def __on_simulation_begins(self, sender: SimulatorHandler) -> None:
         self.__info = {k: 0 for k in self.__info.keys()}
+        self.__simulator = sender
 
     def __on_simulation_ends(self, sender: SimulatorHandler) -> None:
         """ Compute the average. """
@@ -216,10 +202,11 @@ class SchedulerMonitor(Monitor):
 
     def __on_job_completed(self, sender: Job) -> None:
         """ Get job statistics. """
+        assert self.__simulator
         if not sender.is_finished:
             return
 
-        self.__info['makespan'] = self._simulator.current_time
+        self.__info['makespan'] = self.__simulator.current_time
         self.__info['mean_slowdown'] += sender.slowdown
         self.__info['mean_pp_slowdown'] += sender.per_processor_slowdown
         self.__info['mean_stretch'] += sender.stretch
@@ -248,13 +235,9 @@ class HostMonitor(Monitor):
     This monitor collects statistics about the time each host spent on each
     of its possible states. Moreover, it computes the total energy consumed
     and the total number of state switches.
-
-    Args:
-        simulator: the simulator handler.
     """
 
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        super().__init__(simulator)
+    def __init__(self) -> None:
         self.__last_state: dict = {}
         self.__info: dict = {
             'time_idle':  0,
@@ -267,16 +250,15 @@ class HostMonitor(Monitor):
             'nb_switches': 0,
             'nb_computing_machines': 0,
         }
+        self.__simulator: Optional[SimulatorHandler] = None
 
         subscribe(
             self.__on_simulation_begins,
             SimulatorEvent.SIMULATION_BEGINS,
-            self._simulator,
         )
         subscribe(
             self.__on_simulation_ends,
             SimulatorEvent.SIMULATION_ENDS,
-            self._simulator,
         )
 
         subscribe(self.__on_host_state_changed, HostEvent.STATE_CHANGED)
@@ -301,29 +283,31 @@ class HostMonitor(Monitor):
         return pd.DataFrame.from_dict(self.__info, orient='index').T
 
     def __on_simulation_begins(self, sender: SimulatorHandler) -> None:
-        assert self._simulator.platform
-        t_now = self._simulator.current_time
+        self.__simulator = sender
+        assert self.__simulator.platform
+        t_now = self.__simulator.current_time
 
         self.__last_state = {}
-        for h in self._simulator.platform:
+        for h in self.__simulator.platform:
             self.__last_state[h.id] = (t_now, h.power, h.state, h.pstate)
 
         self.__info = {k: 0 for k in self.__info.keys()}
-        self.__info['nb_computing_machines'] = self._simulator.platform.size
+        self.__info['nb_computing_machines'] = self.__simulator.platform.size
 
     def __on_simulation_ends(self, sender: SimulatorHandler) -> None:
-        assert self._simulator.platform
-        for h in self._simulator.platform:
+        assert self.__simulator and self.__simulator.platform
+        for h in self.__simulator.platform:
             self.__update_info(h)
 
     def __on_host_state_changed(self, sender: Host) -> None:
         self.__update_info(sender)
 
     def __update_info(self, h: Host) -> None:
+        assert self.__simulator
         t_start, power, state, pstate = self.__last_state[h.id]
 
         # Update Info
-        time_spent = self._simulator.current_time - t_start
+        time_spent = self.__simulator.current_time - t_start
         energy_consumption = time_spent * (power or 0)
         energy_wasted = 0
 
@@ -350,7 +334,7 @@ class HostMonitor(Monitor):
             self.__info['nb_switches'] += 1
 
         # Update Last State
-        t_now = self._simulator.current_time
+        t_now = self.__simulator.current_time
         self.__last_state[h.id] = (t_now, h.power, h.state, h.pstate)
 
 
@@ -360,26 +344,25 @@ class SimulationMonitor(Monitor):
     This monitor collects statistics about the simulation. It merges the
     statistics from the Scheduler Monitor with the Host Monitor.
 
-    Args:
-        simulator: the simulator handler.
+
+
     """
 
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        super().__init__(simulator)
-        self.__sched_monitor = SchedulerMonitor(simulator)
-        self.__hosts_monitor = HostMonitor(simulator)
+    def __init__(self) -> None:
+        self.__sched_monitor = SchedulerMonitor()
+        self.__hosts_monitor = HostMonitor()
         self.__sim_start_time = self.__simulation_time = -1.
 
         subscribe(
             self.__on_simulation_begins,
             SimulatorEvent.SIMULATION_BEGINS,
-            self._simulator,
+
         )
 
         subscribe(
             self.__on_simulation_ends,
             SimulatorEvent.SIMULATION_ENDS,
-            self._simulator,
+
         )
 
     @property
@@ -414,12 +397,12 @@ class HostStateSwitchMonitor(Monitor):
     This monitor collects statistics about the host state switches. It'll
     record the number of hosts on each state when any host switches its state.
 
-    Args:
-        simulator: the simulator handler.
+
+
     """
 
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        super().__init__(simulator)
+    def __init__(self) -> None:
+        self.__simulator: Optional[SimulatorHandler] = None
         self.__last_host_state: dict = {}
         self.__info: dict = {
             'time': [],
@@ -432,7 +415,7 @@ class HostStateSwitchMonitor(Monitor):
         subscribe(
             self.__on_simulation_begins,
             SimulatorEvent.SIMULATION_BEGINS,
-            self._simulator,
+
         )
 
         subscribe(self.__on_host_state_changed, HostEvent.STATE_CHANGED)
@@ -455,24 +438,26 @@ class HostStateSwitchMonitor(Monitor):
         return pd.DataFrame.from_dict(self.__info)
 
     def __on_simulation_begins(self, sender: SimulatorHandler) -> None:
-        assert self._simulator.platform
+        self.__simulator = sender
+        assert self.__simulator.platform
         self.__last_host_state = {}
         self.__info = {k: [0] for k in self.__info.keys()}
 
         # Update initial state
-        self.__info['time'][-1] = self._simulator.current_time
-        for h in self._simulator.platform:
+        self.__info['time'][-1] = self.__simulator.current_time
+        for h in self.__simulator.platform:
             key = self.__get_key_from_state(h.state)
             self.__last_host_state[h.id] = key
             self.__info[key][-1] += 1
 
     def __on_host_state_changed(self, sender: Host) -> None:
-        if self.__info['time'][-1] != self._simulator.current_time:
+        assert self.__simulator
+        if self.__info['time'][-1] != self.__simulator.current_time:
             # Update new info from last one
             for k in self.__info.keys():
                 self.__info[k].append(self.__info[k][-1])
 
-            self.__info['time'][-1] = self._simulator.current_time
+            self.__info['time'][-1] = self.__simulator.current_time
 
         # Update info
         old_key = self.__last_host_state[sender.id]
@@ -503,20 +488,19 @@ class HostPowerStateSwitchMonitor(Monitor):
     This monitor collects statistics about the host power state switches. It'll
     record an entry when a host switches its power state.
 
-    Args:
-        simulator: the simulator handler.
+
+
     """
 
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        super().__init__(simulator)
-
+    def __init__(self) -> None:
+        self.__simulator: Optional[SimulatorHandler] = None
         self.__last_pstate_id: dict = {}
         self.__info: dict = {'time': [], 'machine_id': [], 'new_pstate': []}
 
         subscribe(
             self.__on_simulation_begins,
             SimulatorEvent.SIMULATION_BEGINS,
-            self._simulator,
+
         )
 
         subscribe(self.__on_host_power_state_changed,
@@ -541,18 +525,19 @@ class HostPowerStateSwitchMonitor(Monitor):
         return pd.DataFrame.from_dict(self.__info)
 
     def __on_simulation_begins(self, sender: SimulatorHandler) -> None:
-        assert self._simulator.platform
+        self.__simulator = sender
+        assert self.__simulator.platform
 
         self.__info = {k: [] for k in self.__info.keys()}
 
-        hosts = [h for h in self._simulator.platform if h.pstate]
+        hosts = [h for h in self.__simulator.platform if h.pstate]
 
         self.__last_pstate_id = {
             h.id: h.pstate.id for h in hosts}  # type:ignore
 
         # Record initial state
         for p, hs in groupby(hosts, key=lambda h: h.pstate.id):  # type:ignore
-            self.__info['time'].append(self._simulator.current_time)
+            self.__info['time'].append(self.__simulator.current_time)
             procset = ProcSet(*[h.id for h in hs])
             self.__info['machine_id'].append(str(procset))
             self.__info['new_pstate'].append(p)
@@ -562,6 +547,7 @@ class HostPowerStateSwitchMonitor(Monitor):
             return
 
         assert sender.pstate
+        assert self.__simulator
 
         if self.__last_pstate_id[sender.id] == sender.pstate.id:
             return
@@ -574,14 +560,14 @@ class HostPowerStateSwitchMonitor(Monitor):
         else:
             new_pstate_id = sender.pstate.id
 
-        if self.__info['time'][-1] == self._simulator.current_time and self.__info['new_pstate'][-1] == new_pstate_id:
+        if self.__info['time'][-1] == self.__simulator.current_time and self.__info['new_pstate'][-1] == new_pstate_id:
             # Update last record
             procset = ProcSet.from_str(self.__info['machine_id'][-1])
             procset.update(ProcSet(sender.id))
             self.__info['machine_id'][-1] = str(procset)
         else:
             # Record new state
-            self.__info['time'].append(self._simulator.current_time)
+            self.__info['time'].append(self.__simulator.current_time)
             self.__info['machine_id'].append(str(sender.id))
             self.__info['new_pstate'].append(new_pstate_id)
 
@@ -592,8 +578,8 @@ class ConsumedEnergyMonitor(Monitor):
     This monitor collects energy statistics. It'll record the platform consumed
     energy when a job starts/finish or a host changes its state/power state.
 
-    Args:
-        simulator: the simulator handler.
+
+
 
     Attributes:
         POWER_STATE_TYPE: A signal that a power state switch ocurred.
@@ -605,8 +591,8 @@ class ConsumedEnergyMonitor(Monitor):
     JOB_STARTED_TYPE = "s"
     JOB_COMPLETED_TYPE = "e"
 
-    def __init__(self, simulator: SimulatorHandler) -> None:
-        super().__init__(simulator)
+    def __init__(self) -> None:
+        self.__simulator: Optional[SimulatorHandler] = None
         self.__last_state: dict = {}
         self.__info: dict = {
             'time': [], 'energy': [], 'event_type': [], 'wattmin': [], 'epower': []
@@ -615,7 +601,7 @@ class ConsumedEnergyMonitor(Monitor):
         subscribe(
             self.__on_simulation_begins,
             SimulatorEvent.SIMULATION_BEGINS,
-            self._simulator,
+
         )
 
         subscribe(self.__handle_job_started_event, JobEvent.STARTED)
@@ -647,12 +633,13 @@ class ConsumedEnergyMonitor(Monitor):
         return pd.DataFrame.from_dict(self.__info)
 
     def __on_simulation_begins(self, sender: SimulatorHandler) -> None:
-        assert self._simulator.platform
-        t_now = self._simulator.current_time
+        self.__simulator = sender
+        assert self.__simulator.platform
+        t_now = self.__simulator.current_time
         self.__info = {k: [] for k in self.__info.keys()}
 
         self.__last_state = {
-            h.id: (t_now, h.power, h.pstate) for h in self._simulator.platform
+            h.id: (t_now, h.power, h.pstate) for h in self.__simulator.platform
         }
 
     def __handle_job_started_event(self, sender: Host) -> None:
@@ -665,11 +652,11 @@ class ConsumedEnergyMonitor(Monitor):
         self.__update_info(event_type=self.POWER_STATE_TYPE)
 
     def __update_info(self, event_type: str) -> None:
-        assert self._simulator.platform
+        assert self.__simulator and self.__simulator.platform
         consumed_energy = wattmin = epower = 0.
-        for h in self._simulator.platform:
+        for h in self.__simulator.platform:
             t_start, power, pstate = self.__last_state[h.id]
-            t_now = self._simulator.current_time
+            t_now = self.__simulator.current_time
             wattage = power or 0
 
             epower += wattage
@@ -680,7 +667,7 @@ class ConsumedEnergyMonitor(Monitor):
             self.__last_state[h.id] = (t_now, h.power, h.pstate)
 
         consumed_energy += self.__info['energy'][-1] if self.__info['energy'] else 0
-        self.__info["time"].append(self._simulator.current_time)
+        self.__info["time"].append(self.__simulator.current_time)
         self.__info["energy"].append(consumed_energy)
         self.__info["event_type"].append(event_type)
         self.__info["wattmin"].append(wattmin)
